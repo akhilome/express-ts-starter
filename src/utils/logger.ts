2a350d90818aa4a2b01a86bec948acb4f8ae847c
Kizito Akhilome <kizito@akhilo.me>
2021-07-17 01:20:35 +0100

    chore: do deps upgrade

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -20,10 +20,10 @@ export class Logger {
       const errorStack = errorObject.stack;
 
       /*
-        node environment checks can happen here to 
+        node environment checks can happen here to
         know whether or not to dump errors to stdout.
 
-        For ex., we could decide to only print to stdout 
+        For ex., we could decide to only print to stdout
         when process.env.NODE_ENV is set to development.
       */
 
